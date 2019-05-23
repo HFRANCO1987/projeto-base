8ac79ca246fcc7db74e0ad32fd0de7783ff29606
Henrique Santiago <henriquesantiagofranco@gmail.com>
2019-05-23 15:45:11 -0300

    [ALT] exclusão de arquivos desnecessários

---
+++ src/main/java/br/com/projetobase/dao/JPAUtil.java
@@ -0,0 +1,24 @@
+package br.com.projetobase.dao;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("bdmysql");
+
+	@Produces
+	@RequestScoped
+	public EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public void close(@Disposes EntityManager em) {
+		em.close();
+	}
+
+}
---
+++ src/main/java/br/com/projetobase/services/ClienteSrv.java
@@ -0,0 +1,9 @@
+package br.com.projetobase.services;
+
+import java.io.Serializable;
+
+public class ClienteSrv implements Serializable {
+
+	private static final long serialVersionUID = -7346517870891397976L;
+
+}
